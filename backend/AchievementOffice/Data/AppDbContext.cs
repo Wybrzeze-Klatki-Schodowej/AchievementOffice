edf69f9665b40c6f5808462c2931272c777ef899
Franek Jawor <39954561+mistrzegiptu@users.noreply.github.com>
2026-05-04 11:01:05 +0200

    [AOF-28] Add db config and ef core (#2)

---
+++ backend/AchievementOffice/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace AchievementOffice.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ backend/AchievementOffice/Extensions/DatabaseConfigurationExtension.cs
@@ -0,0 +1,32 @@
+﻿using AchievementOffice.Data;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+
+namespace AchievementOffice.Extensions
+{
+    public static class DatabaseConfigurationExtension 
+    {
+        public static IServiceCollection SetupDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connnectionString = BuildConnectionString(configuration);
+
+            services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connnectionString));
+
+            return services;
+        }
+
+        private static string BuildConnectionString(IConfiguration configuration)
+        {
+            var connectionStringBuilder = new NpgsqlConnectionStringBuilder
+            {
+                Port = configuration.GetValue<int>("DatabaseSettings:Port", 5432),
+                Host = configuration.GetValue<string>("DatabaseSettings:Host") ?? throw new ArgumentNullException("Host is missing"),
+                Database = configuration.GetValue<string>("DatabaseSettings:Database") ?? throw new ArgumentNullException("Database is missing"),
+                Username = configuration.GetValue<string>("DatabaseSettings:Username") ?? throw new ArgumentNullException("User is missing"),
+                Password = configuration.GetValue<string>("DatabaseSettings:Password") ?? throw new ArgumentNullException("Password is missing")
+            };
+
+            return connectionStringBuilder.ConnectionString;
+        }
+    }
+}
