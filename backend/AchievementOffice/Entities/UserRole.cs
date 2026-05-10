d4434248e351521f0ba99a1d3fd261f51ae14302
Mateusz Ryś <mateuszrys97@gmail.com>
2026-05-10 20:00:55 +0200

    Feature/aof 41 user entity (#7)
    * [AOF-41] User role entity added
    
    * [AOF-41] User entity added
    
    * [AOF-41] User details added and some config done
    
    * Update backend/AchievementOffice/Data/AppDbContext.cs
    
    Co-authored-by: Franek Jawor <39954561+mistrzegiptu@users.noreply.github.com>
    
    * [AOF-41] Some fixes
    
    * [AOF-41] Csproj file change
    
    ---------
    
    Co-authored-by: Franek Jawor <39954561+mistrzegiptu@users.noreply.github.com>

---
+++ backend/AchievementOffice/Entities/UserRole.cs
@@ -0,0 +1,10 @@
+﻿namespace AchievementOffice.Entities
+{
+    public class UserRole
+    { 
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+
+        public ICollection<User> Users { get; } = new HashSet<User>();
+    }
+}
