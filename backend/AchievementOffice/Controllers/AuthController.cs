18dc6967c5581700079f00445b3d77d457ff5283
Lynxman21 <mateuszrys97@gmail.com>
2026-05-12 21:36:29 +0200

    [AOF-35] Session init

---
+++ backend/AchievementOffice/Controllers/AuthController.cs
@@ -0,0 +1,75 @@
+﻿using AchievementOffice.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AchievementOffice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginModel loginModel)
+        {
+            if (loginModel is null) return BadRequest("Invalid request");
+
+            //TODO: Add to Claim list elements of User Entity
+            if (loginModel.UserName == "Test" && loginModel.Password == "1234")
+            {
+                var issuer = _configuration["JwtConf:Issuer"];
+                var audience = _configuration["JwtConf:Audience"];
+                var signingKey = _configuration["JwtConf:SigningKey"];
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey!));
+                var signCred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                var opts = new JwtSecurityToken(
+                    issuer: issuer,
+                    audience: audience,
+                    claims: new List<Claim>(),
+                    expires: DateTime.Now.AddHours(1),
+                    signingCredentials: signCred
+                );
+
+                var tokenStr = new JwtSecurityTokenHandler().WriteToken(opts);
+
+                var coockieOpts = new CookieOptions
+                {
+                    HttpOnly = true,
+                    Secure = false,
+                    SameSite = SameSiteMode.Strict,
+                    Expires = DateTime.Now.AddHours(1)
+                };
+
+                Response.Cookies.Append("X-jwt-token", tokenStr, coockieOpts);
+                return Ok();
+            }
+            return Unauthorized("Invalid login or password");
+        }
+
+        [HttpPost("logout")]
+        public IActionResult Logout()
+        {
+            Response.Cookies.Delete("X-jwt-token");
+            return Ok();
+        }
+
+        [Authorize]
+        [HttpGet("mock-data")]
+        public IActionResult MockGet()
+        {
+            var username = User.Identity?.Name ?? "Nieznajomy";
+            return Ok(new { message = $"Wszedłeś! Twój token działa. Cześć, {username}!" });
+        }
+    }
+}
--- backend/AchievementOffice/Program.cs
+++ backend/AchievementOffice/Program.cs
@@ -1,8 +1,61 @@
 using AchievementOffice.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var frontUrl = builder.Configuration["CorseSettings:FrontUrl"];
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("ReactPolicy", policy =>
+    {
+        policy.WithOrigins(frontUrl!)
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+
+var issuer = builder.Configuration["JwtConf:Issuer"];
+var audience = builder.Configuration["JwtConf:Audience"];
+var signingKey = builder.Configuration["JwtConf:SigningKey"];
+
+builder.Services.AddAuthentication(opt =>
+{
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = issuer,
+        ValidAudience = audience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey!))
+    };
+
+    options.Events = new JwtBearerEvents
+    {
+        OnMessageReceived = context =>
+        {
+            if (context.Request.Cookies.ContainsKey("X-jwt-token"))
+            {
+                context.Token = context.Request.Cookies["X-jwt-token"];
+            }
+
+            return Task.CompletedTask;
+        }
+    };
+});
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -22,6 +75,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors("ReactPolicy");
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
