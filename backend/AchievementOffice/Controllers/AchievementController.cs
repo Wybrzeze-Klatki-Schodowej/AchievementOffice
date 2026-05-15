656ae6f2ae8cd07b04c61a94a5faf0563b664082
Mateusz <mateusz.nnowak2003@gmail.com>
2026-05-15 20:03:21 +0200

    [AOF-54] Add AchievementApprove controller endpoints and DTOs

--- backend/AchievementOffice/Controllers/AchievementController.cs
+++ backend/AchievementOffice/Controllers/AchievementController.cs
@@ -64,4 +64,19 @@ public class AchievementController : ControllerBase
 
         return NoContent();
     }
+
+    [HttpPost( "{id:guid}/approve" )]
+    public async Task<ActionResult<AchievementApproveResponseDto>> Approve(Guid id, CreateAchievementApproveDto dto)
+    {
+        var userId = Guid.Parse( "00000000-0000-0000-0000-000000000001" );
+        var approve = await _achievementService.ApproveAsync( userId, dto );
+        return Ok( approve );
+    }
+
+    [HttpGet( "{id:guid}/approvals" )]
+    public async Task<ActionResult<List<AchievementApproveResponseDto>>> GetApprovals(Guid id)
+    {
+        var approvals = await _achievementService.GetApprovalsAsync( id );
+        return Ok( approvals );
+    }
 }
