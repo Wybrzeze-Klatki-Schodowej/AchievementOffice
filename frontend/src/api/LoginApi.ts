11e88c6799be098b0b7d519a92ed65ecc549e615
Mateusz Ryś <mateuszrys97@gmail.com>
2026-05-19 16:17:52 +0200

    Feature/aof 56 logging out (#25)
    * [AOF-56] Add logout endpoint
    
    * [AOF-56] Logout button added
    
    * [AOF-67] Added endpoint to get current user
    
    * [AOF-56] Login page changes
    
    ---------
    
    Co-authored-by: Jakub Zając <jakubtomaszkrzysztof@gmail.com>

--- frontend/src/api/LoginApi.ts
+++ frontend/src/api/LoginApi.ts
@@ -38,3 +38,28 @@ export async function checkAuth(): Promise<boolean> {
         return false;
     }
 }
+
+export async function logout(): Promise<void> {
+    try {
+        await fetch(API_URL + "/logout", {
+        method: "POST",
+        credentials: "include"
+        });
+
+        window.location.href = "/login";
+    } catch (err) {
+        console.error("Error occurred while logging out:", err);
+    }
+}
+
+export const getCurrentUser = async() => {
+    const res = await fetch(API_URL + "/me", {
+        credentials: "include"
+    });
+
+    if (!res.ok) {
+        throw new Error("Failed to fetch current user");
+    }
+
+    return res.json();
+}
