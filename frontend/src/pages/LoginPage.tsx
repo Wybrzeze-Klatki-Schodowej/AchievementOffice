d7e425965dbebac921f659f5ce1b8c0447eb2f00
Mistrz Egiptu <mistrzegiptu@gmail.com>
2026-05-19 23:08:05 +0200

    Revert "[AOF-38] Add register button and page"
    This reverts commit e4cd45d4161cfaa6908bbd5a60e84fca19d26d65.

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,7 +1,6 @@
 import LoginForm from '../components/login/LoginForm';
 import { useState } from 'react';
-import type { LoginDTO, RegisterDTO } from '../api/LoginApi';
-import { login, register } from '../api/LoginApi';
+import {type LoginDTO, login } from '../api/LoginApi';
 import { useNavigate } from 'react-router-dom';
 import './LoginPage.css';
 
@@ -12,37 +11,22 @@ interface LoginPageProps {
 export default function LoginPage({ onLogin }: LoginPageProps) {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState<string | null>(null);
-    const [successMessage, setSuccessMessage] = useState<string | null>(null);
-    const [mode, setMode] = useState<'login' | 'register'>('login');
     const navigate = useNavigate();
 
-    const handleSubmit = async (data: LoginDTO | RegisterDTO) => {
+    const handleSubmit = async (data: LoginDTO) => {
         setIsLoading(true);
         setError(null);
-        setSuccessMessage(null);
 
         try {
-            if (mode === 'login') {
-                await login(data as LoginDTO);
-                onLogin();
-                navigate('/');
-            } else {
-                await register(data as RegisterDTO);
-                setSuccessMessage('Registration successful. Please log in.');
-                setMode('login');
-            }
+            await login(data);
+            onLogin();
+            navigate('/');
         } catch (err: any) {
             setError(err.message);
         } finally {
             setIsLoading(false);
         }
-    };
-
-    const toggleMode = () => {
-        setError(null);
-        setSuccessMessage(null);
-        setMode((prevMode) => (prevMode === 'login' ? 'register' : 'login'));
-    };
+    }
 
     return (
         <div className='login-page'>
@@ -50,15 +34,8 @@ export default function LoginPage({ onLogin }: LoginPageProps) {
                 <h1 className='top-left-text'>Share your success</h1>
             </div>
             <div className='form-container'>
-                <h2 className='login-title'>{mode === 'login' ? 'Login' : 'Register'}</h2>
-                <LoginForm
-                    isLoading={isLoading}
-                    mode={mode}
-                    onSubmit={handleSubmit}
-                    onModeChange={toggleMode}
-                    error={error}
-                    successMessage={successMessage}
-                />
+                <h2 className='login-title'>Login</h2>
+                <LoginForm isLoading={isLoading} onSubmit={handleSubmit} error={error}></LoginForm>
             </div>
         </div>
     );
