a8f5c9c06887a4702ebbb373eb1d6a73e4d0a756
Mistrz Egiptu <mistrzegiptu@gmail.com>
2026-06-06 01:40:46 +0200

    [AOF-116] Add entities

---
+++ backend/AchievementOffice/Data/Configurations/GroupUserRoleConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using AchievementOffice.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AchievementOffice.Data.Configurations
+{
+    public class GroupUserRoleConfiguration : IEntityTypeConfiguration<GroupUserRole>
+    {
+        public void Configure(EntityTypeBuilder<GroupUserRole> builder)
+        {
+            builder.ToTable("GroupUserRole");
+
+            builder.HasKey(gur => gur.GroupUserRoleId);
+
+            builder.Property(gur => gur.GroupUserRoleId)
+                .HasColumnName("group_user_role_id");
+
+            builder.Property(gur => gur.Name)
+                .HasColumnName("group_user_role_name")
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+    }
+}
---
+++ backend/AchievementOffice/Entities/GroupUserRole.cs
@@ -0,0 +1,8 @@
+﻿namespace AchievementOffice.Entities
+{
+    public class GroupUserRole
+    {
+        public Guid GroupUserRoleId { get; set; }
+        public required string Name { get; set; }
+    }
+}
