5952ad30b2dcd224ff182adc9265b9c87eb04027
Lynxman <mateuszrys97@gmail.com>
2026-06-09 17:46:33 +0200

    [AOF-127] Service done

---
+++ backend/AchievementOffice/Controllers/RankController.cs
@@ -0,0 +1,23 @@
+﻿using AchievementOffice.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using RouteAttribute = Microsoft.AspNetCore.Components.RouteAttribute;
+
+namespace AchievementOffice.Controllers
+{
+    [ApiController]
+    [Route("/api/ranks")]
+    [Authorize]
+    public class RankController
+    {
+        private readonly IRankService _rankService;
+
+        public RankController(IRankService rankService)
+        {
+            _rankService = rankService;
+        }
+
+
+    }
+}
---
+++ backend/AchievementOffice/Models/RankRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace AchievementOffice.Models
+{
+    public class RankRequest
+    {
+        public string? Name { get; set; }
+        public decimal? Multiplier { get; set; }
+    }
+}
