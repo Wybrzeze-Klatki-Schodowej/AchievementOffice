1b51b8a76c0745454b099a037d372e1a9dce055f
Jakub Zając <jakubtomaszkrzysztof@gmail.com>
2026-06-12 15:45:40 +0200

    [AOF-129] Repaired migration

--- backend/AchievementOffice/Migrations/20260612133802_SyncWithDev.Designer.cs
+++ backend/AchievementOffice/Migrations/20260612133802_SyncWithDev.Designer.cs
@@ -12,8 +12,8 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace AchievementOffice.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260610002644_NewMigration")]
-    partial class NewMigration
+    [Migration("20260612133802_SyncWithDev")]
+    partial class SyncWithDev
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -361,6 +361,55 @@ namespace AchievementOffice.Migrations
                     b.ToTable("Ranks", (string)null);
                 });
 
+            modelBuilder.Entity("AchievementOffice.Entities.Shoutout", b =>
+                {
+                    b.Property<Guid>("ShoutoutId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("uuid")
+                        .HasColumnName("shoutout_id");
+
+                    b.Property<DateTime>("CreatedAt")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("timestamp with time zone")
+                        .HasColumnName("created_at")
+                        .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                    b.Property<DateTime?>("DeletedAt")
+                        .HasColumnType("timestamp with time zone")
+                        .HasColumnName("deleted_at");
+
+                    b.Property<string>("Description")
+                        .HasMaxLength(500)
+                        .HasColumnType("character varying(500)")
+                        .HasColumnName("description");
+
+                    b.Property<Guid>("ReceiverId")
+                        .HasColumnType("uuid")
+                        .HasColumnName("receiver_id");
+
+                    b.Property<Guid>("SenderId")
+                        .HasColumnType("uuid")
+                        .HasColumnName("sender_id");
+
+                    b.Property<string>("Title")
+                        .IsRequired()
+                        .HasMaxLength(100)
+                        .HasColumnType("character varying(100)")
+                        .HasColumnName("title");
+
+                    b.Property<DateTime>("UpdatedAt")
+                        .HasColumnType("timestamp with time zone")
+                        .HasColumnName("updated_at");
+
+                    b.HasKey("ShoutoutId");
+
+                    b.HasIndex("ReceiverId");
+
+                    b.HasIndex("SenderId");
+
+                    b.ToTable("Shoutouts", (string)null);
+                });
+
             modelBuilder.Entity("AchievementOffice.Entities.User", b =>
                 {
                     b.Property<Guid>("Id")
@@ -622,6 +671,25 @@ namespace AchievementOffice.Migrations
                     b.Navigation("AchievementVerificationRequest");
 
                     b.Navigation("User");
+                });
+
+            modelBuilder.Entity("AchievementOffice.Entities.Shoutout", b =>
+                {
+                    b.HasOne("AchievementOffice.Entities.User", "Receiver")
+                        .WithMany()
+                        .HasForeignKey("ReceiverId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("AchievementOffice.Entities.User", "Sender")
+                        .WithMany()
+                        .HasForeignKey("SenderId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Receiver");
+
+                    b.Navigation("Sender");
                 });
 
             modelBuilder.Entity("AchievementOffice.Entities.User", b =>
