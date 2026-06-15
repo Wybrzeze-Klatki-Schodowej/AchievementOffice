23d01c00b7dec84fced2d2e01edf4892f6da4733
Lynxman <mateuszrys97@gmail.com>
2026-06-15 16:40:08 +0200

    [AOF-8] Controller added

--- backend/AchievementOffice/Controllers/RankingController.cs
+++ backend/AchievementOffice/Controllers/RankingController.cs
@@ -1,8 +1,12 @@
-﻿using AchievementOffice.Services;
+﻿using AchievementOffice.Entities;
+using AchievementOffice.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AchievementOffice.Controllers
 {
+    [ApiController]
+    [Route("api/ranking")]
     public class RankingController : ControllerBase
     {
         private readonly IRankingService _rankingService;
@@ -12,6 +16,26 @@ namespace AchievementOffice.Controllers
             _rankingService = rankingService;
         }
 
+        [Authorize]
+        [HttpGet("users")]
+        public async Task<IActionResult> GetUserRanking()
+        {
+            var users = await _rankingService.GetUserRanking();
+            if (!users.IsSuccess)
+                return BadRequest(new { message = users.ErrorMessage });
 
+            return Ok(users.Value);
+        }
+
+        [Authorize]
+        [HttpGet("groups")]
+        public async Task<IActionResult> GetGroupRanking()
+        {
+            var groups = await _rankingService.GetGroupRanking();
+            if (!groups.IsSuccess)
+                return BadRequest(new { message = groups.ErrorMessage });
+
+            return Ok(groups.Value);
+        }
     }
 }
