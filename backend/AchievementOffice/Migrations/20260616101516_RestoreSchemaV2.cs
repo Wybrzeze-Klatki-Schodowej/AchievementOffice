e1595c5ecbde1becd4a7640f19c99ee0c4b53a8a
Mateusz <mateusz.nnowak2003@gmail.com>
2026-06-16 13:33:58 +0200

    [AOF-141] Admin delete shoutout added

---
+++ backend/AchievementOffice/Migrations/20260616101516_RestoreSchemaV2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AchievementOffice.Migrations
+{
+    /// <inheritdoc />
+    public partial class RestoreSchemaV2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
